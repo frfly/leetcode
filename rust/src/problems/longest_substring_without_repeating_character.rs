7db90ae3a4affbe066ac54f74603ced46842abd5
frfly <ozimantius@gmail.com>
2022-10-13 02:17:35 +0600

    Tune memory usage in third problem

--- rust/src/problems/longest_substring_without_repeating_character.rs
+++ rust/src/problems/longest_substring_without_repeating_character.rs
@@ -1,13 +1,13 @@
 /// https://leetcode.com/problems/longest-substring-without-repeating-characters/
 /// Given a string s, find the length of the longest substring without repeating characters.
 use super::common::solution::Solution;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 impl Solution {
     pub fn length_of_longest_substring(str: String) -> i32 {
         let mut max_len = 0;
         let mut len = 0;
-        let mut map = HashMap::new();
+        let mut map = BTreeMap::new();
         for (i, chr) in str.chars().enumerate() {
             let old_pos = map.entry(chr).or_insert(i);
             if *old_pos == i || i - *old_pos > len {
